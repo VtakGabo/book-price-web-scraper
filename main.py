1c7550c58c20ccbdc6e806af778550b0c96843c4
Gabriel <128378978+VtakGabo@users.noreply.github.com>
2026-03-12 15:55:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from scraper import scrape_books
 from analyzer import analyze_books
 
-print("Starting scraper...")
+print("Starting scraper")
 scrape_books()
 
-print("Starting analysis...")
+print("Starting analysis")
 analyze_books()
 
-print("Project finished!")
+
+print("Project finished")
